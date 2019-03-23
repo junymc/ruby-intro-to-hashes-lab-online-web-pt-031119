7661683887a50bdd0e23d6940c78e68428ac2a64
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-23 20:25:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,15 +21,25 @@ def monopoly_with_second_tier
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
-				:four_pieces_owned => 200}
+				:four_pieces_owned => 200
+			 }
+		},
+		:names => {
+			:reading_railroad => {
+				"mortgage_value" => "$100"
 			},
-			:names => {
-				:reading_railroad => {"mortgage_value" => "$100"},
-				:pennsylvania_railroad => {"mortgage_value" => "$200"},
-				:b_and_o_railroad => {"mortgage_value" => "$400"},
-				:shortline => {"mortgage_value" => "$800"}
+			:pennsylvania_railroad => {
+				"mortgage_value" => "$200"
+			},
+			:b_and_o_railroad => {
+				"mortgage_value" => "$400"
+			},
+			:shortline => {
+				"mortgage_value" => "$800"
 			}
-	}#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+		}
+	}
+}#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
